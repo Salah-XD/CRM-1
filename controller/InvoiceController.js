2ce31d7b93c22e39747b9f3735567e33170c9edc
Arun Sharma <75688559+Arundevloper@users.noreply.github.com>
2024-05-30 14:26:18 +0530

    Quotion designed

--- controller/InvoiceController.js
+++ controller/InvoiceController.js
@@ -20,7 +20,28 @@ export const generatePDF = async (req, res) => {
       numberOfFoodHandlers: 5,
       proposalNumber: "PRO123",
       proposalDate: "2024-05-30",
+      formatNumber: "12344",
+      issueDate: "2024-05-30",
+      versionNumber: "1",
+
+      total: "100",
+      gstRate: "90",
+      location: "Head Office",
+      audit: "Internal Audit",
+      frequency: "Quarterly",
+      quantity: 1,
+      unitCost: 500,
+      total: 500,
+
+      totalAmountWithoutTax: 500,
+      tax: 90,
+      totalAmountWithTax: 590,
     };
+    // Calculate GST amount
+    const gstAmount = (dynamicData.total * dynamicData.gstRate) / 100;
+
+    // Calculate overall total including GST
+    const overallTotal = dynamicData.total + gstAmount;
 
     // Read HTML template from file
     const htmlTemplate = await fs.readFile(
@@ -29,7 +50,7 @@ export const generatePDF = async (req, res) => {
     );
 
     // Read the image file and convert it to base64 encoding
-    const imagePath = path.join(__dirname, "templates", "shoe.jpg");
+    const imagePath = path.join(__dirname, "templates", "logo.jpg");
     const imageData = await fs.readFile(imagePath, { encoding: "base64" });
 
     // Inject dynamic data into HTML template
@@ -44,7 +65,24 @@ export const generatePDF = async (req, res) => {
       .replace("{{numberOfFoodHandlers}}", dynamicData.numberOfFoodHandlers)
       .replace("{{proposalNumber}}", dynamicData.proposalNumber)
       .replace("{{proposalDate}}", dynamicData.proposalDate)
-      .replace("{{imageData}}", imageData);
+      .replace("{{issueDate}}", dynamicData.issueDate)
+      .replace("{{formatNumber}}", dynamicData.formatNumber)
+      .replace("{{versionNumber}}", dynamicData.versionNumber)
+
+      .replace("{{total}}", dynamicData.total)
+      .replace("{{gst}}", dynamicData.gstRate)
+      .replace("{{overallTotal}}", overallTotal)
+      .replace("{{location}}", dynamicData.location)
+      .replace("{{audit}}", dynamicData.audit)
+      .replace("{{frequnecy}}", dynamicData.frequency)
+      .replace("{{quantity}}", dynamicData.quantity)
+      .replace("{{unitCost}}", dynamicData.unitCost)
+
+      .replace("{{imageData}}", imageData)
+      .replace("{{toatlAmountWithoutTax}}", dynamicData.totalAmountWithoutTax)
+      .replace("{{tax}}", dynamicData.tax)
+      .replace("{{toatlAmountWithTax}}", dynamicData.totalAmountWithTax);
+
 
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
